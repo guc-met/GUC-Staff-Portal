b4fb1bc4428aa5f9d1fcbc0dd2707ae9eb3662ee
Eslam Sabry <Eslam.221@gmail.com>
2021-01-10 22:19:09 +0200

    Schedule takes a JSON Object ro render

---
+++ src/example-components/ScheduleSlot/index.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+
+import {Button} from '@material-ui/core';
+
+const noRad = {borderRadius:"0"};
+
+export default function LivePreviewExample(props) {
+    return (
+        <Fragment>
+            <td className="p-0 m-0">
+                { props.slotContents.map(slot => (
+                    <Button variant="contained" color="" className="mb-1" fullWidth style={noRad}>
+                    <span className="btn-wrapper--icon">
+
+                    </span>
+
+                    <span className="btn-wrapper--label">{slot.Location}</span>
+                    <span className="btn-wrapper--label">{' '}|{' '}</span>
+                    <span className="btn-wrapper--label">{slot.Staff}</span>
+                </Button>
+                )) }
+            </td>
+        </Fragment>
+    );
+}
--- src/example-components/ScheduleTable/index.js
+++ src/example-components/ScheduleTable/index.js
@@ -6,15 +6,27 @@ import {
     TableContainer,
     TableHead,
     TableRow,
-    Paper
+    Button
   } from '@material-ui/core';
+import ScheduleSlot from '../ScheduleSlot'
+import ScheduleDay from '../ScheduleDay'
 
+const sched = { 'Saturday': { '1st': [ { Location:'C3.102', Staff: 'N/A' }, { Location:'C5.208', Staff:'Eithar Elhinamy' }, { Location:'C3.305', Staff: 'Belal Medhat' } ], '2nd': [ { Location:'H11', Staff: 'Mervat Aboelkheir' }, { Location:'H16', Staff: 'Amr Elmougy' } ], '3rd': [ ], '4th': [ ], '5th': [ { Location:'H14', Staff: 'N/A' }, { Location:'D4.302', Staff: 'Mahmoud Gamal' }, { Location:'H13', Staff: 'Kalabala Person' }, { Location:'D4.305', Staff: 'Mahmoud Ahmed' } ] }, 'Sunday': { '1st': [ { Location:'C3.102', Staff: 'N/A' }, { Location:'C5.208', Staff:'Eithar Elhinamy' }, { Location:'C3.305', Staff: 'Belal Medhat' } ], '2nd': [ { Location:'H11', Staff: 'Mervat Aboelkheir' }, { Location:'H16', Staff: 'Amr Elmougy' } ], '3rd': [ ], '4th': [ ], '5th': [ { Location:'H14', Staff: 'N/A' }, { Location:'D4.302', Staff: 'Mahmoud Gamal' }, { Location:'H13', Staff: 'Kalabala Person' }, { Location:'D4.305', Staff: 'Mahmoud Ahmed' } ] }, 'Monday': { '1st': [ { Location:'C3.102', Staff: 'N/A' }, { Location:'C5.208', Staff:'Eithar Elhinamy' }, { Location:'C3.305', Staff: 'Belal Medhat' } ], '2nd': [ { Location:'H11', Staff: 'Mervat Aboelkheir' }, { Location:'H16', Staff: 'Amr Elmougy' } ], '3rd': [ ], '4th': [ ], '5th': [ { Location:'H14', Staff: 'N/A' }, { Location:'D4.302', Staff: 'Mahmoud Gamal' }, { Location:'H13', Staff: 'Kalabala Person' }, { Location:'D4.305', Staff: 'Mahmoud Ahmed' } ] }, 'Tuesday': { '1st': [ { Location:'C3.102', Staff: 'N/A' }, { Location:'C5.208', Staff:'Eithar Elhinamy' }, { Location:'C3.305', Staff: 'Belal Medhat' } ], '2nd': [ { Location:'H11', Staff: 'Mervat Aboelkheir' }, { Location:'H16', Staff: 'Amr Elmougy' } ], '3rd': [ ], '4th': [ ], '5th': [ { Location:'H14', Staff: 'N/A' }, { Location:'D4.302', Staff: 'Mahmoud Gamal' }, { Location:'H13', Staff: 'Kalabala Person' }, { Location:'D4.305', Staff: 'Mahmoud Ahmed' } ] }, 'Wednesday': { '1st': [ { Location:'C3.102', Staff: 'N/A' }, { Location:'C5.208', Staff:'Eithar Elhinamy' }, { Location:'C3.305', Staff: 'Belal Medhat' } ], '2nd': [ { Location:'H11', Staff: 'Mervat Aboelkheir' }, { Location:'H16', Staff: 'Amr Elmougy' } ], '3rd': [ ], '4th': [ ], '5th': [ { Location:'H14', Staff: 'N/A' }, { Location:'D4.302', Staff: 'Mahmoud Gamal' }, { Location:'H13', Staff: 'Kalabala Person' }, { Location:'D4.305', Staff: 'Mahmoud Ahmed' } ] }, 'Thursday': { '1st':[], '2nd':[], '3rd':[], '4th':[], '5th':[] }, 'Friday': { '1st':[], '2nd':[], '3rd':[], '4th':[], '5th':[] } };
 
+const noRad = {borderRadius:"0"};
 
 export default function LivePreviewExample() {
     return (
     <Fragment>
         <table className="table table-striped table-hover table-bordered mb-4">
+            <colgroup>
+                <col style={{width:'16.6%'}}/>
+                <col style={{width:'16.6%'}}/>
+                <col style={{width:'16.6%'}}/>
+                <col style={{width:'16.6%'}}/>
+                <col style={{width:'16.6%'}}/>
+                <col style={{width:'16.6%'}}/>
+            </colgroup>
                 <thead className="thead-light">
                     <tr>
                         <th scope="col">#</th>
@@ -27,56 +39,28 @@ export default function LivePreviewExample() {
                 </thead>
                 <tbody>
                     <tr>
-                        <th scope="row">Saturday</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
+                        <th scope="row"> Saturday</th>
+                        <ScheduleDay day={sched['Saturday']} />
                     </tr>
                     <tr>
-                        <th scope="row">Sunday</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
+                        <th scope="row"> Sunday</th>
+                        <ScheduleDay day={sched['Sunday']} />
                     </tr>
                     <tr>
-                        <th scope="row">Monday</th>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                        <td colSpan="2">Larry the Bird</td>
+                        <th scope="row"> Monday</th>
+                        <ScheduleDay day={sched['Monday']} />
                     </tr>
                     <tr>
-                        <th scope="row">Tuesday</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
+                        <th scope="row"> Tuesday</th>
+                        <ScheduleDay day={sched['Tuesday']} />
                     </tr>
                     <tr>
-                        <th scope="row">Wednesday</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
+                        <th scope="row"> Wednesday</th>
+                        <ScheduleDay day={sched['Wednesday']} />
                     </tr>
                     <tr>
-                        <th scope="row">Thursday</th>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr> 
-                        <th scope="row"> Friday</th>
-                        <td> </td>
-                        <td> </td>
-                        <td> </td>
-                        <td> </td>
-                        <td> </td>
+                        <th scope="row"> Thursday</th>
+                        <ScheduleDay day={sched['Thursday']} />
                     </tr>
                 </tbody>
             </table>
