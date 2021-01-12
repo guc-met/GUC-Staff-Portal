d482b6a003850e489d9eedbd633d4197777b7926
khaled 99 elshabrawy <khaled.elshabrawy@hotmail.com>
2021-01-12 23:31:25 +0200

    modified sent and received requests

--- src/example-components/myRequestsAC/index.js
+++ src/example-components/myRequestsAC/index.js
@@ -28,6 +28,9 @@ import LastPageIcon from '@material-ui/icons/LastPage';
 import BasicButtonGroup from '../../example-components/BasicGroupButton';
 import { AirlineSeatReclineNormalRounded } from '@material-ui/icons';
 
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
 let token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtoYWxlZC5nYW1lZEBndWMuY29tIiwiaWQiOiJhYy0xIiwibmFtZSI6ImtoYWxlZCBnYW1lZCIsInJvbGUiOiJIT0QiLCJpYXQiOjE2MTAzMDE4NDZ9.zGhbG2WJPamGpNbdPFtYFa-q2NLuD4ksVe7EePpU5Js'
 
 
@@ -104,26 +107,6 @@ TablePaginationActions.propTypes = {
   rowsPerPage: PropTypes.number.isRequired
 };
 
-function createData(name, calories, fat) {
-  return { name, calories, fat };
-}
-
-const rows = [
-  createData('Cupcake', 305, 3.7),
-  createData('Donut', 452, 25.0),
-  createData('Eclair', 262, 16.0),
-  createData('Frozen yoghurt', 159, 6.0),
-  createData('Gingerbread', 356, 16.0),
-  createData('Honeycomb', 408, 3.2),
-  createData('Ice cream sandwich', 237, 9.0),
-  createData('Jelly Bean', 375, 0.0),
-  createData('KitKat', 518, 26.0),
-  createData('Lollipop', 392, 0.2),
-  createData('Marshmallow', 318, 0),
-  createData('Nougat', 360, 19.0),
-  createData('Oreo', 437, 18.0)
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
-
 const useStyles2 = makeStyles({
   table: {
     minWidth: 500,
@@ -150,7 +133,13 @@ export default function LivePreviewExample() {
   const [requestType,setRequestType]=React.useState('replacementRequests')
   const [requestsFiltered, setRequestsFiltered] = React.useState([])
   const [requests, setRequests] = React.useState([[],[],[],[],[]])
-  const [dummy,setDummy]=React.useState(0)
+
+  const [requestViewRec,setRequestViewRec]=React.useState('all')
+  const [requestTypeRec,setRequestTypeRec]=React.useState('replacementRequests')
+  const [requestsFilteredRec, setRequestsFilteredRec] = React.useState([])
+
+  const [dummy, setDummy] = React.useState(0);
+  const [open, setOpen] = React.useState([false,'success','all good']);
 
   React.useEffect(() => {
 
@@ -163,7 +152,7 @@ export default function LivePreviewExample() {
         {
           headers: {
             //'Content-Type': 'application/json',
-           token: token,  //to be added
+            token: localStorage.getItem('UserToken'),  //to be added
           // token
             //    'auth-token': localStorage.getItem('user'),
           }
@@ -177,17 +166,32 @@ export default function LivePreviewExample() {
         return [];
       })
       
-      setRequests(result)      
+      await setRequests(result)     
     }
     fetchData();
   }, []);
-  
 
-  // console.log('1111')
-  // console.log(requestView)
-  // console.log(requestType)
-  // console.log(requestsFiltered)
-
+const reRender=()=>{
+  axios
+      .get(
+        'http://localhost:3001/ac/viewStatusOfRequests',
+        
+        {
+          headers: {
+            //'Content-Type': 'application/json',
+            token: localStorage.getItem('UserToken'),  //to be added
+          // token
+            //    'auth-token': localStorage.getItem('user'),
+          }
+        }
+      )
+      .then(function(response) {
+        setRequests(response.data)
+      })
+      .catch(function(error) {
+        console.log(error.response);
+      }) 
+}
 
   const emptyRows =
     rowsPerPage - Math.min(rowsPerPage, requestsFiltered.length - page * rowsPerPage);
@@ -214,6 +218,20 @@ export default function LivePreviewExample() {
         return 'primary'
     }
   }
+  const setButtonColorRec=(button,type)=>{
+    if(type==1){
+      if(requestViewRec==button)
+        return 'inherit'
+      else
+        return 'secondary'
+    }else{
+      
+      if(requestTypeRec==button)
+        return 'inherit'
+      else
+        return 'primary'
+    }
+  }
   const onClickFunc=(data,type)=>{
     let viewx=requestView
     let typex=requestType
@@ -234,13 +252,155 @@ export default function LivePreviewExample() {
       case 'changeDayOffRequests':setRequestsFiltered(changeDay);break;
       case 'slotLinkingRequests':setRequestsFiltered(slot);break;
     }
-    console.log(requestView)
-    //setDummy(dummy+1)
+  }
+  const onClickFuncRec=(data,type)=>{
+    let viewx=requestViewRec
+    let typex=requestTypeRec
+    if(type==1){
+      viewx=data
+      setRequestViewRec(data)
+    }else{
+      typex=data
+      setRequestTypeRec(data)
+    }
+    let replacement=(viewx=='all')?requests[1]:requests[1].filter(e=>e.statusInst==viewx||e.statusHod==viewx)
+    setRequestsFilteredRec(replacement)
+  }
+  const setSeen=(bool)=>{
+    if(!bool)
+      return 'bold'
+      else
+      return 'normal'
+  }
+  const onClickAccRej=(id,date,type)=>{
+    axios
+    .put(
+      'http://localhost:3001/ac/replacementRequestAccReg',
+      {
+        senderId:id,
+        Date:date,
+        statusInst:type
+      },
+      {
+        headers: {
+          token: localStorage.getItem('UserToken'),
+        }
+      }
+    )
+    .then(function(response) {
+      //console.log(response)
+      if(response.status!=200)//that's an error
+        setOpen([true,"error",response.data.err]);
+      else{
+        reRender()
+        setOpen([true,"success",response.data.msg]);
+      }
+     })
+
+    .catch(function(error) {
+      console.log(error.response)
+      setOpen([true,"error",error.response.data.err]);
+    });
+    setDummy(dummy+1)
   }
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+     // setOpen(true);
+      return;
+    }
+
+    setOpen([false,open[1],open[2]]);
+  };
+
+
+  const chooseRow0=(row,type)=>{
+    let seen=(type==1)?row.SeenSender:row.SeenReceiver
+    switch(requestType){
+      case 'slotLinkingRequests':return <TableCell style={{fontWeight:setSeen(seen)}}>{checkUndefiend('Course: ',row.courseId)}</TableCell>
+
+    }
+  }
+  const chooseRow1=(row,type)=>{
+    let seen=(type==1)?row.SeenSender:row.SeenReceiver
+    switch(requestType){
+      case 'replacementRequests':return <TableCell style={{fontWeight:setSeen(seen)}}>{checkUndefiend('Date: ',row.Date)}</TableCell>
+      case 'leaveRequests':return <TableCell style={{fontWeight:setSeen(seen)}}>{checkUndefiend('Start Date: ',row.startDate)}</TableCell>
+      case 'changeDayOffRequests':return <TableCell style={{fontWeight:setSeen(seen)}}>{checkUndefiend('New Day: ',row.newday)}</TableCell>
+      case 'slotLinkingRequests':return <TableCell style={{fontWeight:setSeen(seen)}}>{checkUndefiend('Day: ',row.day)}</TableCell>
+    }
+  }
+  const chooseRow2=(row,type)=>{
+    let seen=(type==1)?row.SeenSender:row.SeenReceiver
+    let send=(type==1)?row.receiverId:row.senderId
+    switch(requestType){
+      case 'replacementRequests':return <TableCell style={{fontWeight:setSeen(seen)}}>{checkUndefiend('Colleague ID: ',send)}</TableCell>
+      case 'leaveRequests':return <TableCell style={{fontWeight:setSeen(seen)}}>{checkUndefiend('Type of Leave: ',row.typeOfLeave)}</TableCell>
+      case 'slotLinkingRequests':return <TableCell style={{fontWeight:setSeen(seen)}}>{checkUndefiend('Location: ',row.location)}</TableCell>
+    }
+  }
+  const checkUndefiend=(tag,element)=>{
+    if(element)
+      return tag+element
+    else
+      return element
+  }
+  const sendOne=(row,type)=>{
+    let send=(type==1)?row.receiverId:row.senderId
+    switch(requestType){
+      case 'replacementRequests':return checkUndefiend('Colleague ID: ',send)
+      case 'leaveRequests':return checkUndefiend('Type of Leave: ',row.typeOfLeave)
+      case 'changeDayOffRequests':return checkUndefiend('New Day: ',row.newday)
+      case 'slotLinkingRequests':return checkUndefiend('Course: ',row.courseId)
+    }
+  }
+  const sendTwo=(row)=>{
+    switch(requestType){
+      case 'replacementRequests':return checkUndefiend('Colleague Request Status: ',row.statusInst)
+      case 'leaveRequests':return checkUndefiend('Start Date: ',row.startDate)
+      case 'changeDayOffRequests':return checkUndefiend('Status: ',row.status)
+      case 'slotLinkingRequests':return checkUndefiend('Status: ',row.status)
+    }
+  }
+  const sendThree=(row)=>{
+    switch(requestType){
+      case 'replacementRequests':return checkUndefiend('Head of Department Status: ',row.statusHod)
+      case 'leaveRequests':return checkUndefiend('Period of Leave: ',row.periodOfLeave)
+      case 'changeDayOffRequests':return checkUndefiend('Reason of Change: ',row.reasonOfChange)
+      case 'slotLinkingRequests':return checkUndefiend('Slot: ',row.slot)
+    }
+  }
+  const sendFour=(row)=>{
+    switch(requestType){
+      case 'replacementRequests':return checkUndefiend('Reason: ',row.reason)
+      case 'leaveRequests':return checkUndefiend('Document: ',row.document)
+      case 'changeDayOffRequests':return checkUndefiend('Comment: ',row.comment)
+      case 'slotLinkingRequests':return checkUndefiend('Day: ',row.day)
+    }
+  }
+  const sendFive=(row)=>{
+    switch(requestType){
+      case 'replacementRequests':return checkUndefiend('Date: ',row.Date)
+      case 'leaveRequests':return checkUndefiend('Reason: ',row.reason)
+      case 'slotLinkingRequests':return checkUndefiend('Location: ',row.location)
+    }
+  }
+  const sendSix=(row)=>{
+    switch(requestType){
+      case 'replacementRequests':return checkUndefiend('Slot: ',row.slot)
+      case 'leaveRequests':return checkUndefiend('Compensating Day: ',row.compensatingDay)
+    }
+  }
+  const sendSeven=(row)=>{
+    switch(requestType){
+      case 'leaveRequests':return checkUndefiend('Status: ',row.status)
+    }
+  }
+  
   return (
-      <Fragment>
-        <div className={classes3.root}>
+      <>
+    <Fragment><h3>Sent Requests</h3>
+    <div className={classes3.root}>
       <ButtonGroup variant="contained" color="secondary" aria-label="contained primary button group">
         <Button color={setButtonColor('all',1)} onClick={() =>onClickFunc('all',1)}>All</Button>
         <Button color={setButtonColor('pending',1)} onClick={() =>onClickFunc('pending',1)}>Pending</Button>
@@ -254,7 +414,6 @@ export default function LivePreviewExample() {
         <Button color={setButtonColor('changeDayOffRequests',2)} onClick={()=>onClickFunc('changeDayOffRequests',2)}>Change Day Off</Button>
       </ButtonGroup>
     </div>
-    <Fragment><h3>Sent Requests</h3>
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="custom pagination table">
           <TableBody>
@@ -262,13 +421,16 @@ export default function LivePreviewExample() {
               ? requestsFiltered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               : requestsFiltered
             ).map((row,index) => (
-              <TableRow key={index}>
-                <TableCell><OpenDialog status={row.senderId}></OpenDialog></TableCell>
-                <TableCell component="th" scope="row" align="left">
+              <TableRow key={index} >
+              <TableCell component="th" scope="row" align="left" style={{fontWeight:setSeen(row.SeenSender)}}>
                 {index+1}
-                </TableCell>
-                <TableCell >{row.Date}</TableCell>
-                <TableCell >{row.slot}</TableCell>
+              </TableCell>
+              <TableCell><OpenDialog one={sendOne(row,1)} two={sendTwo(row)} three={sendThree(row)}
+              four={sendFour(row)} five={sendFive(row)} six={sendSix(row)} seven={sendSeven(row)}></OpenDialog></TableCell>
+              {chooseRow0(row,1)} 
+              {chooseRow1(row,1)} 
+              {chooseRow2(row,1)} 
+              <TableCell style={{fontWeight:setSeen(row.SeenSender)}}>{checkUndefiend('slot: ',row.slot)}</TableCell>
               </TableRow>
             ))}
 
@@ -283,7 +445,7 @@ export default function LivePreviewExample() {
               <TablePagination
                 rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
                 colSpan={3}
-                count={rows.length}
+                count={requestsFiltered.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 SelectProps={{
@@ -302,20 +464,39 @@ export default function LivePreviewExample() {
     <br/>
     <br/>
     <Fragment><h3>Received Requests</h3>
+    <div className={classes3.root}>
+      <ButtonGroup variant="contained" color="secondary" aria-label="contained primary button group">
+        <Button color={setButtonColorRec('all',1)} onClick={() =>onClickFuncRec('all',1)}>All</Button>
+        <Button color={setButtonColorRec('pending',1)} onClick={() =>onClickFuncRec('pending',1)}>Pending</Button>
+        <Button color={setButtonColorRec('accepted',1)} onClick={() =>onClickFuncRec('accepted',1)}>Accepted</Button>
+        <Button color={setButtonColorRec('rejected',1)} onClick={() =>onClickFuncRec('rejected',1)}>Rejected</Button>
+      </ButtonGroup>
+      <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+        <Button color={setButtonColorRec('replacementRequests',2)} onClick={()=>onClickFuncRec('replacementRequests',2)}>Replacement</Button>
+        <Button disabled={true} color={setButtonColorRec('leaveRequests',2)} onClick={()=>onClickFuncRec('leaveRequests',2)}>Leave</Button>
+        <Button disabled={true} color={setButtonColorRec('slotLinkingRequests',2)} onClick={()=>onClickFuncRec('slotLinkingRequests',2)}>Slot Linking</Button>
+        <Button disabled={true} color={setButtonColorRec('changeDayOffRequests',2)} onClick={()=>onClickFuncRec('changeDayOffRequests',2)}>Change Day Off</Button>
+      </ButtonGroup>
+    </div>
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="custom pagination table">
           <TableBody>
             {(rowsPerPage > 0
-              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              : rows
-            ).map(row => (
-              <TableRow key={row.name}>
-                <TableCell><OpenDialog></OpenDialog></TableCell>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell >{row.calories}</TableCell>
-                <TableCell >{row.fat}</TableCell>
+              ? requestsFilteredRec.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : requestsFilteredRec
+            ).map((row,index) => (
+              <TableRow key={index} >
+              <TableCell component="th" scope="row" align="left" style={{fontWeight:setSeen(row.SeenReceiver)}}>
+                {index+1}
+              </TableCell>
+              <TableCell><OpenDialog one={sendOne(row,2)} two={sendTwo(row)} three={sendThree(row)}
+              four={sendFour(row)} five={sendFive(row)} six={sendSix(row)} seven={sendSeven(row)}></OpenDialog></TableCell>
+              {chooseRow0(row,2)} 
+              {chooseRow1(row,2)} 
+              {chooseRow2(row,2)} 
+              <TableCell style={{fontWeight:setSeen(row.SeenReceiver)}}>{checkUndefiend('slot: ',row.slot)}</TableCell>
+              <TableCell> <Button className="m-2 btn text-success" onClick={()=>onClickAccRej(row.senderId,row.Date,'accepted')}>Accept</Button></TableCell>
+              <TableCell><Button className="m-2 btn text-danger" onClick={()=>onClickAccRej(row.senderId,row.Date,'rejected')}>Reject</Button></TableCell>
               </TableRow>
             ))}
 
@@ -330,7 +511,7 @@ export default function LivePreviewExample() {
               <TablePagination
                 rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
                 colSpan={3}
-                count={rows.length}
+                count={requestsFilteredRec.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 SelectProps={{
@@ -346,6 +527,11 @@ export default function LivePreviewExample() {
         </Table>
       </TableContainer>
     </Fragment>
-    </Fragment>
+    <Snackbar open={open[0]} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical:'bottom', horizontal :'center'}}>
+      <MuiAlert variant='filled' onClose={handleClose} severity={open[1]}>
+        {open[2]}
+      </MuiAlert>
+    </Snackbar>
+    </>
   );
 }
