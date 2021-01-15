2b64d40c88fac0c59537aaf2f2f72f2a97a2a774
Ahmed Al Arousi <ahmed.ahmed02@gmail.com>
2021-01-15 23:20:06 +0200

    last

--- src/example-components/ScheduleSlot/index.js
+++ src/example-components/ScheduleSlot/index.js
@@ -1,17 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import {Button} from '@material-ui/core';
-import OneSlot from './oneSlot'
-const noRad = {borderRadius:"0"};
+import { Button } from "@material-ui/core";
+
+const noRad = { borderRadius: "0" };
 
 export default function LivePreviewExample(props) {
-    return (
-        <Fragment>
-            <td className="p-2 m-0">
-                { props.slotContents.map(slot => (
-                    <OneSlot />
-                )) }
-            </td>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <td className="p-2 m-0">
+        {props.slotContents.map(slot => (
+          <Button
+            variant="contained"
+            color=""
+            className="mb-1"
+            fullWidth
+            style={noRad}
+          >
+            <span className="btn-wrapper--icon"></span>
+
+            <span className="btn-wrapper--label">{slot.Location}</span>
+            <span className="btn-wrapper--label"> | </span>
+            <span className="btn-wrapper--label">{slot.Staff}</span>
+          </Button>
+        ))}
+      </td>
+    </Fragment>
+  );
 }
