a71a9d4c69175465bed02de51e0f95bd7f084e0b
Eslam Sabry <Eslam.221@gmail.com>
2021-01-15 17:02:22 +0200

    CrsSchd View

---
+++ src/example-components/ScheduleSlot/oneSlot.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+
+import {Button} from '@material-ui/core';
+import {
+    TableBody,
+    Table,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Button
+  } from '@material-ui/core';
+
+export default function LivePreviewExample(props) {
+    return (
+        <Fragment>
+            <Button variant="contained" color="" className="mb-1" fullWidth style={noRad}>
+                    <span className="btn-wrapper--icon">
+
+                    </span>
+
+                    <span className="btn-wrapper--label">{slot.Location}</span>
+                    <span className="btn-wrapper--label">{' '}|{' '}</span>
+                    <span className="btn-wrapper--label">{slot.Staff}</span>
+                </Button>
+                
+
+        </Fragment>
+    );
+}
